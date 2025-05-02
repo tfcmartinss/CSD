a15fb411f2bf4f04d3f675f72666fbbb8e090821
VIDCOELH <duarte.coelho@zeiss.com>
2025-05-02 16:46:03 +0100

    digital signatures nas mensagens trocadas entre client e server

--- csd-project/ledger/src/main/java/com/ledger/controller/LedgerController.java
+++ csd-project/ledger/src/main/java/com/ledger/controller/LedgerController.java
@@ -2,9 +2,14 @@ package com.ledger.controller;
 
 import com.ledger.model.AccountRequest;
 import com.ledger.model.TransferRequest;
+import com.ledger.service.Client;
 import com.ledger.service.LedgerService;
+import com.ledger.service.Server;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -12,44 +17,151 @@ import org.springframework.web.bind.annotation.*;
 public class LedgerController {
 
     private final LedgerService ledgerService;
+    private final Client client;
+    private final Server server;
 
     @PostMapping("/create_account")
-    public String createAccount(@RequestBody AccountRequest request) {
-        return ledgerService.createAccount(request);
+    public Map<String, String> createAccount(@RequestBody AccountRequest request) throws Exception {
+        String signedRequest = client.signData(request.toString());
+        boolean isValid = server.verifySignature(request.toString(), signedRequest);
+
+        Map<String, String> response = new HashMap<>();
+        if (isValid) {
+            String result = ledgerService.createAccount(request);
+            String signedResponse = server.signData(result);
+
+            response.put("result", result);
+            response.put("signature", signedResponse);
+        } else {
+            response.put("result", "Invalid signature");
+        }
+
+        processServerResponse(response.get("result"), response.get("signature"));
+        return response;
     }
 
     @PostMapping("/transfer_tokens")
-    public String transferTokens(@RequestBody TransferRequest request) {
-        return ledgerService.transferTokens(request);
+    public Map<String, String> transferTokens(@RequestBody TransferRequest request) throws Exception {
+        String signedRequest = client.signData(request.toString());
+        boolean isValid = server.verifySignature(request.toString(), signedRequest);
+
+        Map<String, String> response = new HashMap<>();
+        if (isValid) {
+            String result = ledgerService.transferTokens(request);
+            String signedResponse = server.signData(result);
+
+            response.put("result", result);
+            response.put("signature", signedResponse);
+        } else {
+            response.put("result", "Invalid signature");
+        }
+
+        processServerResponse(response.get("result"), response.get("signature"));
+        return response;
     }
 
     @GetMapping("/list_transactions/{email}")
-    public String listTransactions(@PathVariable String email) {
-        return ledgerService.listTransactions(email);
+    public Map<String, String> listTransactions(@PathVariable String email) throws Exception {
+        String result = ledgerService.listTransactions(email);
+
+        String signedResponse = server.signData(result);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("result", result);
+        response.put("signature", signedResponse);
+
+        processServerResponse(response.get("result"), response.get("signature"));
+        return response;
     }
 
     @GetMapping("/get_balance/{email}")
-    public int getBalance(@PathVariable String email) {
-        return ledgerService.getBalance(email);
+    public Map<String, String> getBalance(@PathVariable String email) throws Exception {
+        String result = String.valueOf(ledgerService.getBalance(email));
+
+        String signedResponse = server.signData(result);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("result", result);
+        response.put("signature", signedResponse);
+
+        processServerResponse(response.get("result"), response.get("signature"));
+        return response;
     }
 
     @DeleteMapping("/delete_account/{email}")
-    public String deleteAccount(@PathVariable String email) {
-        return ledgerService.deleteAccount(email);
+    public Map<String, String> deleteAccount(@PathVariable String email) throws Exception {
+        String result = ledgerService.deleteAccount(email);
+
+        String signedResponse = server.signData(result);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("result", result);
+        response.put("signature", signedResponse);
+
+        processServerResponse(response.get("result"), response.get("signature"));
+        return response;
     }
 
     @PutMapping("/update_account")
-    public String updateAccount(@RequestBody AccountRequest request) {
-        return ledgerService.updateAccount(request);
+    public Map<String, String> updateAccount(@RequestBody AccountRequest request) throws Exception {
+        String signedRequest = client.signData(request.toString());
+        boolean isValid = server.verifySignature(request.toString(), signedRequest);
+
+        Map<String, String> response = new HashMap<>();
+        if (isValid) {
+            String result = ledgerService.updateAccount(request);
+            String signedResponse = server.signData(result);
+
+            response.put("result", result);
+            response.put("signature", signedResponse);
+        } else {
+            response.put("result", "Invalid signature");
+        }
+
+        processServerResponse(response.get("result"), response.get("signature"));
+        return response;
     }
 
     @GetMapping("/get_account_info/{email}")
-    public String getAccountInfo(@PathVariable String email) {
-        return ledgerService.getAccountInfo(email);
+    public Map<String, String> getAccountInfo(@PathVariable String email) throws Exception {
+        String result = ledgerService.getAccountInfo(email);
+
+        String signedResponse = server.signData(result);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("result", result);
+        response.put("signature", signedResponse);
+
+        processServerResponse(response.get("result"), response.get("signature"));
+        return response;
     }
 
     @PostMapping("/validate_account")
-    public boolean validateAccount(@RequestBody AccountRequest request) {
-        return ledgerService.validateAccount(request);
+    public Map<String, String> validateAccount(@RequestBody AccountRequest request) throws Exception {
+        String signedRequest = client.signData(request.toString());
+        boolean isValid = server.verifySignature(request.toString(), signedRequest);
+
+        Map<String, String> response = new HashMap<>();
+        if (isValid) {
+            String result = String.valueOf(ledgerService.validateAccount(request));
+            String signedResponse = server.signData(result);
+
+            response.put("result", result);
+            response.put("signature", signedResponse);
+        } else {
+            response.put("result", "Invalid signature");
+        }
+
+        processServerResponse(response.get("result"), response.get("signature"));
+        return response;
+    }
+
+    private void processServerResponse(String responseData, String responseSignature) throws Exception {
+        boolean isValid = client.verifySignature(responseData, responseSignature);
+        if (isValid) {
+            System.out.println("Response is valid: " + responseData);
+        } else {
+            System.out.println("Invalid response signature!");
+        }
     }
 }
---
+++ csd-project/ledger/src/main/java/com/ledger/service/Server.java
@@ -0,0 +1,67 @@
+package com.ledger.service;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.util.Base64;
+
+@Component
+public class Server {
+
+    private PrivateKey privateKey;
+    private PublicKey publicKey;
+
+    @Value("${server.keystore.name}")
+    private String serverKeystoreName;
+    @Value("${client.keystore.name}")
+    private String clientKeystoreName;
+    @Value("${server.ssl.key-store-type}")
+    private String serverKeystoreType;
+    @Value("${server.keystore.alias}")
+    private String serverKeystoreAlias;
+    @Value("${client.keystore.alias}")
+    private String clientKeystoreAlias;
+    @Value("${server.keystore.password}")
+    private String serverKeystorePassword;
+    @Value("${client.keystore.password}")
+    private String clientKeystorePassword;
+    @Value("${server.ssl.key-store-algorithm}")
+    private String serverKeystoreAlgorithm;
+
+    @PostConstruct
+    public void init() throws Exception {
+        KeyStore keyStore = KeyStore.getInstance(this.serverKeystoreType);
+        keyStore.load(getClass().getClassLoader().getResourceAsStream(this.serverKeystoreName), this.serverKeystorePassword.toCharArray());
+        this.privateKey = (PrivateKey) keyStore.getKey(this.serverKeystoreAlias, this.serverKeystorePassword.toCharArray());
+
+        keyStore.load(getClass().getClassLoader().getResourceAsStream(this.clientKeystoreName), this.clientKeystorePassword.toCharArray());
+        this.publicKey = keyStore.getCertificate(this.clientKeystoreAlias).getPublicKey();
+    }
+
+    public String signData(String data) throws Exception {
+        Signature signature = Signature.getInstance(this.serverKeystoreAlgorithm);
+        signature.initSign(this.privateKey);
+        signature.update(data.getBytes());
+        byte[] signedData = signature.sign();
+        return Base64.getEncoder().encodeToString(signedData);
+    }
+
+    public boolean verifySignature(String data, String signedData) throws Exception {
+        try {
+            Signature signature = Signature.getInstance(serverKeystoreAlgorithm);
+            signature.initVerify(this.publicKey);
+            signature.update(data.getBytes());
+            byte[] signedBytes = Base64.getDecoder().decode(signedData);
+            return signature.verify(signedBytes);
+        } catch (Exception e) {
+            System.out.println("Exception during signature verification: " + e.getMessage());
+            e.printStackTrace();
+            throw e;
+        }
+    }
+}
